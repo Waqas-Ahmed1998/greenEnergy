501adc613ed0219fc11c36d6051c2d188424bda1
Waqas-Ahmed1998 <nizamaniwaqas4@gmail.com>
2021-02-02 22:53:27 +0500

    modified landing mobile vh

--- app.js
+++
@@ -1,9 +0,0 @@
-const navSlide = () => {
-  const burgur = document.querySelector('#navbar .burgur-manue');
-  const navItems = document.querySelector('#navbar .nav-links');
-
-  burgur.addEventListener(click, () => {
-    navItems.classList.toggle('nav-active');
-  });
-};
-navSlide();
